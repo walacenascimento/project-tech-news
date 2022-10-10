485c5837d76c342ccaa5c60d6667a6240ce6646c
walacenascimento <wlcens@hotmail.com>
2022-10-10 20:28:57 -0300

    Implementa a função Fetch, resolve o requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,9 +1,29 @@
 # Requisito 1
+
+import time
+import requests
+# from parsel import Selector
+# from tech_news.database import create_news
+
+
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        if response.status_code != 200:
+            return None
+
+        return response.text
+    except requests.ReadTimeout:
+        return None
 
 
-# Requisito 2
+# Requisito
+
+
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
 
