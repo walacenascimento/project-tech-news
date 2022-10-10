47d76b7238e447676b39a1b235434e8a10c01b1f
walacenascimento <wlcens@hotmail.com>
2022-10-10 20:49:10 -0300

    Implementa a função scrape_next_page_link, resolve o requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,6 +37,10 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css(".pagination a.next::attr(href)").get()
+
+    return url
 
 
 # Requisito 4
