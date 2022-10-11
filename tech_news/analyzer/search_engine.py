103969f5c3efbd94cb2d7176f94b508ccc05afad
walacenascimento <wlcens@hotmail.com>
2022-10-11 20:40:49 -0300

    Implementa a função search_by_title, resolve o requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
+import re
+from tech_news.database import search_news
+
 # Requisito 6
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    query = {"title": re.compile(title, re.IGNORECASE)}
+    filtered_news = search_news(query)
+    result = []
+
+    for news in filtered_news:
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 7
