85b6038e5fbc729a91fd7321dd05cd97457f3e44
walacenascimento <wlcens@hotmail.com>
2022-10-12 11:32:33 -0300

    Cria a função analyzer_menu, resolve o requisito 12

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,20 @@
 # Requisito 12
 def analyzer_menu():
     """Seu código deve vir aqui"""
+    option_menu = input(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por tag;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair."
+    )
+
+    return option_menu
+
+
+if __name__ == "__main__":
+    analyzer_menu()
