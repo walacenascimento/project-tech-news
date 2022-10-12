50ce4b1adda443d5f7944ae76e918a16fd97e414
walacenascimento <wlcens@hotmail.com>
2022-10-12 10:08:45 -0300

    Implementa a função top_5_news, resolve o requisito10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    all_news = find_news()
+
+    all_sorted_news = sorted(
+        all_news, key=lambda news: (-news["comments_count"], news["title"]))
+
+    result = []
+
+    for news in all_sorted_news[:5]:
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 11
