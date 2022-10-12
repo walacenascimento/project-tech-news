10c4f4a1114718404aa5a7989e8a78deab0e4586
walacenascimento <wlcens@hotmail.com>
2022-10-12 11:17:20 -0300

    Implementando a função top_5_categories, resolve o requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -7,7 +8,8 @@ def top_5_news():
     all_news = find_news()
 
     all_sorted_news = sorted(
-        all_news, key=lambda news: (-news["comments_count"], news["title"]))
+        all_news, key=lambda news: (-news["comments_count"], news["title"])
+    )
 
     result = []
 
@@ -20,3 +22,18 @@ def top_5_news():
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    all_news = find_news()
+    all_categories = []
+
+    for news in all_news:
+        all_categories.append(news["category"])
+    list_5_categories = Counter(all_categories).most_common(5)
+
+    result = []
+
+    for category in sorted(
+        list_5_categories, key=lambda category: (-category[1], category[0])
+    ):
+        result.append(category[0])
+
+    return result
